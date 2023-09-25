ffecee4c4612800a910c1f693e4f252eb6d1cea6
prakash.jhugroo <prakash.jhugroo@agileum.com>
2023-09-25 12:28:31 +0400

    #main

--- src/components/ThemeSwitcher.jsx
+++ src/components/ThemeSwitcher.jsx
@@ -3,8 +3,8 @@ import { Theme, Button } from 'react-daisyui'
 import { themeChange } from 'theme-change';
 
 export default () => {
-    const [theme, setTheme] = useState('dracula');
-    const [themeText, setThemeText] = useState('Dark');
+    const [theme, setTheme] = useState('light');
+    const [themeText, setThemeText] = useState('Light');
     useEffect(() => {
         document.querySelector('html')?.setAttribute('data-theme', localStorage.getItem('theme'))
         localStorage.getItem('theme') == 'dracula' ? setThemeText('Light') : setThemeText('Dark');
@@ -14,10 +14,12 @@ export default () => {
         theme == 'dracula' ? setTheme('light') : setTheme('dracula');
         theme == 'dracula' ? setThemeText('Light') : setThemeText('Dark');
         localStorage.setItem('theme', theme)
+        localStorage.getItem('theme') == 'dracula' ? setThemeText('Light') : setThemeText('Dark');
+        themeChange()
     }
     return (
         <>
-            <Button variant="outline"  onClick={() => { HandleChange(); }}>{themeText}</Button>
+            <Button variant="outline" onClick={() => { HandleChange(); }}>{themeText}</Button>
         </>
     )
 }
